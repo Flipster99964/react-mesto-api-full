9cadf46f6c84ef961fc6f4d606227ac91125ad16
Flipster99964 <lay.12345@yandex.ru>
2022-11-23 18:39:57 +0300

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,15 +12,16 @@ module.exports.corsCheck = (req, res, next) => {
   const { method } = req;
 
   const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Credentials', true);
   }
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
   return next();
