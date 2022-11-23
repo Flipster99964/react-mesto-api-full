c4d0508a62e38737ddcf7ccdf1d2f14eddeae70b
Flipster99964 <lay.12345@yandex.ru>
2022-11-23 18:26:34 +0300

    test

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -14,7 +14,7 @@ module.exports.corsCheck = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
   // Если это предварительный запрос, добавляем нужные заголовки
