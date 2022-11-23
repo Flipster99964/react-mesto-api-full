7e47b779861a1c3ffa408e25e341a0e10c174fb2
Flipster99964 <lay.12345@yandex.ru>
2022-11-23 16:29:38 +0300

    fix

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,8 +1,6 @@
 const corsOptions = {
   origin: [
     'https://flipster99964.students.nomoredomains.club/',
-    'http://flipster99964.students.nomoredomains.club/',
-    'https://web.postman.co',
   ],
   credentials: true,
 };
