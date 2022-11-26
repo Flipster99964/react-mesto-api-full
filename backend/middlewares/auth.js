77b0b4dc17ebb8c4de9d99a57756cb69f5f82cd9
Flipster99964 <lay.12345@yandex.ru>
2022-11-26 16:50:54 +0300

    fix secret key

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
   try {
     // попытаемся верифицировать токен
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new BadAuthError('Необходима авторизация.'));
   }
