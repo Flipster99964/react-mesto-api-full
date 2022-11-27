4f04e7ab26378bb8bc2ca959058b50e4ab986f7b
Flipster99964 <lay.12345@yandex.ru>
2022-11-27 15:40:21 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const BodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
@@ -8,33 +9,8 @@ const routes = require('./routes/routes');
 const cenralErrors = require('./middlewares/central-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const allowedCors = [
-  'https://flipster99964.students.nomoredomains.club',
-  'http://flipster99964.students.nomoredomains.club',
-  'https://api.flipster99964.student.nomoredomains.club/users/me',
-  'http://api.flipster99964.student.nomoredomains.club',
-];
 const { PORT = 3000 } = process.env;
 const app = express();
-function corsCheck(req, res, next) {
-  const { origin } = req.headers;
-  const { method } = req;
-
-  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  return next();
-}
 async function main() {
   try {
     await mongoose.connect('mongodb://localhost:27017/mestodb');
@@ -50,7 +26,9 @@ app.use(BodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(requestLogger); // подключаем логгер запросов
 app.use(helmet());
-app.use(corsCheck);
+app.use(cors({
+  origin: 'https://flipster99964.students.nomoredomains.club',
+}));
 app.use(routes);
 
 app.use(errorLogger); // подключаем логгер ошибок
